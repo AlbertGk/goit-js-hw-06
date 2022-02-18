de6108974ac27c586ad5715fcac7e6cc7dec1c1d
AlbertGk <albertgk01@gmail.com>
2022-02-18 18:18:51 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const arrayWithTags = []
+
+ingredients.forEach((el) => {
+  const listElement = document.createElement("li");
+  listElement.textContent = el;
+  listElement.classList.add("item");
+  arrayWithTags.push(listElement);
+});
+
+
+const parentElement = document.querySelector("ul#ingredients")
+parentElement.append(...arrayWithTags);
