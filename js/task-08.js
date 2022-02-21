b7b418b74b749b448284d7424e75cc32800cafea
AlbertGk <albertgk01@gmail.com>
2022-02-21 13:36:19 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const pageForm = document.querySelector('form.login-form');
 
+function formHandler(event) {
+    event.preventDefault();
+
+    const {
+      elements: { email, password },
+    } = event.currentTarget;
+
+
+    if (email.value === "" || password.value === "") {
+        alert("Proszę uzupełnić wszystkie pola");
+    } else {
+        console.log(`Email: ${email.value}, Password: ${password.value}`);
+        event.currentTarget.reset();
+    }
+}
+
+pageForm.addEventListener('submit', formHandler);
