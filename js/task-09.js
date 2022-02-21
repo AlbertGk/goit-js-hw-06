c1f904a69329f5417e98b66258190a3fd3ee20c4
AlbertGk <albertgk01@gmail.com>
2022-02-21 14:24:26 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyElem = document.querySelector("body");
+const colorName = document.querySelector('span.color')
+const colorChangeButton = document.querySelector("button.change-color");
+
+const colorChanger = () => {
+  colorName.textContent = getRandomHexColor();
+  bodyElem.style.backgroundColor = `${getRandomHexColor()}`;
+}
+
+colorChangeButton.addEventListener('click', colorChanger);
