895efe1e00837bfd639457d4646fa4537c4214a6
AlbertGk <albertgk01@gmail.com>
2022-02-21 16:40:18 +0100

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,40 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const boxesQuantityField = document.querySelector('#controls input');
+const createButton = document.querySelector("#controls button[data-create]");
+const destroyButton = document.querySelector("#controls button[data-destroy]");
+const boxesPlace = document.querySelector('div#boxes')
+
+let amount = 0;
+
+const getBoxesQuantity = (event) => {
+  amount = event.currentTarget.value;
+};
+
+boxesQuantityField.addEventListener("input", getBoxesQuantity);
+
+
+function createBoxes() {
+  let widthAndHeight = 20;
+  
+  for (let i = 1; i <= amount; i++){
+    let singleDiv = document.createElement("div");
+    widthAndHeight += 10;
+    singleDiv.style.width = `${widthAndHeight}px`;
+    singleDiv.style.height = `${widthAndHeight}px`;
+    singleDiv.style.backgroundColor = `${getRandomHexColor()}`;
+    singleDiv.style.margin = '5px'
+    boxesPlace.append(singleDiv);
+  }
+};
+
+createButton.addEventListener("click", createBoxes);
+
+
+function destroyBoxes() {
+  boxesPlace.innerHTML = "";
+};
+
+destroyButton.addEventListener("click", destroyBoxes);
