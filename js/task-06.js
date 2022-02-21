fbf20b4c3e53e4934182cd18fa32464447df7f96
AlbertGk <albertgk01@gmail.com>
2022-02-21 14:36:08 +0100

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,12 @@
 const pageForm = document.querySelector("input#validation-input");
 
 const formValidation = (event) => {
-    if (event.currentTarget.value.length === 6) {
-        pageForm.classList.add('valid');
-        pageForm.classList.remove("invalid");
+    if (event.currentTarget.value.length === Number(pageForm.dataset.length)) {
+      pageForm.classList.add("valid");
+      pageForm.classList.remove("invalid");
     } else {
-        pageForm.classList.add('invalid');
-        pageForm.classList.remove("valid");
+      pageForm.classList.add("invalid");
+      pageForm.classList.remove("valid");
     }
 }
 
